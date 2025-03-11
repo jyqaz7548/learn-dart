3c4097acc4539b5909ce6349fd605ac75c5b60d0
jyqaz7548 <24robot10507@sonline20.sen.go.kr>
2025-03-11 15:34:43 +0900

    Map

---
+++ dictionary.dart
@@ -0,0 +1,3 @@
+void main() {
+  var player = {'name': 'nico', 'xp': 19.99, 'superpower': false};
+}
--- list.dart
+++ list.dart
@@ -1,5 +1,10 @@
 void main() {
-  var giveMeFive = true;
-  List<int> numbers = [1, 2, 3, 4, if (giveMeFive) 5];
-  print(numbers);
+  var oldFriends = ['nico', 'lynn'];
+  var newFriends = [
+    'lweis',
+    'ralph',
+    'darren',
+    for (var friend in oldFriends) "* $friend",
+  ];
+  print(newFriends);
 }
