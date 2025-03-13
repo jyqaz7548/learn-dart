affc29240dcbf58a5c8c87fc8ba637fe3656203d
jyqaz7548 <24robot10507@sonline20.sen.go.kr>
2025-03-14 00:06:30 +0900

    parameter

--- parameters.dart
+++ parameters.dart
@@ -1,1 +1,7 @@
-void main() {}
+String sayHello(String name, int age, [String? country = 'cuba']) =>
+    'Hello $name, you are $age years old from $country';
+
+void main() {
+  sayHello('nico', 12);
+  print(sayHello('nico', 12));
+}
