463f3fbcfee61b2fa3c59a602b8c8f41ae392815
jyqaz7548 <24robot10507@sonline20.sen.go.kr>
2025-03-14 19:43:53 +0900

    흑흑

--- class.dart
+++ class.dart
@@ -1,8 +1,12 @@
+abstract class Human {
+  void walk();
+}
+
 enum Team { red, blue }
 
 enum XPLevel { beginner, medium, pro }
 
-class Player {
+class Player extends Human {
   String name;
   XPLevel xp;
   int age;
@@ -14,6 +18,10 @@ class Player {
     required this.team,
     required this.age,
   });
+
+  void walk() {
+    print("i am walking");
+  }
 
   void sayHello() {
     print("Hi my name is $name");
